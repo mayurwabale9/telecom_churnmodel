47f85f4039867eede57314a178518c375b5527bd
mayurwabale9 <mayurwabale52@gmail.com>
2026-07-30 16:47:05 +0530

    Done

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 
 def data_ingestion():
-    df = pd.read_csv(r'C:\telecom_churnmodel\data\churn.csv')
+    df = pd.read_csv(r'https://raw.githubusercontent.com/mayurwabale9/telecom_churnmodel/refs/heads/main/data/churn.csv')
 
     return df
